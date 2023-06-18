83bc1fe32c55bd0f3b995758a593b92d7f0ad0b2
BuildTools <unconfigured@null.spigotmc.org>
2023-06-17 21:54:28 -0300

    cadastro

--- Api/funcionario.js
+++ Api/funcionario.js
@@ -1,10 +1,25 @@
-// Função para cadastrar um funcionário
-function cadastrarFuncionario() {
+document.addEventListener("DOMContentLoaded", function() {
+  function cadastrarFuncionario(event) {
+    event.preventDefault(); // Impede o envio do formulário e o recarregamento da página
+
     var login = document.getElementById("login").value;
     var senha = document.getElementById("senha").value;
     var nome = document.getElementById("nome-funcionario").value;
     var cpf = document.getElementById("cpf-funcionario").value;
-  
+    var gdprCheckbox = document.getElementById("gdpr-form4-3");
+
+    // Verifica se algum campo está em branco
+    if (login === "" || senha === "" || nome === "" || cpf === "") {
+      alert("Por favor, preencha todos os campos.");
+      return; // Interrompe o cadastro se houver campos em branco
+    }
+
+    // Verifica se a checkbox de consentimento foi marcada
+    if (!gdprCheckbox.checked) {
+      alert("Por favor, aceite os termos de serviço e a política de privacidade.");
+      return; // Interrompe o cadastro se a checkbox não estiver marcada
+    }
+
     // Fazer uma requisição para a API para inserir os dados do funcionário
     fetch("https://faculdadedb-faculdadeapi.yykemf.easypanel.host/api/Funcionario", {
       method: "POST",
@@ -17,10 +32,22 @@ function cadastrarFuncionario() {
       .then((data) => {
         // Lógica após a resposta da API
         console.log(data);
+        if (data.success) {
+          // Cadastro bem-sucedido
+          alert("Funcionário cadastrado com sucesso!");
+          // Redirecionar para a página de login ou outra página adequada
+          window.location.replace("login.html");
+        } else if (data.error) {
+          // Cadastro falhou
+          alert("Erro ao cadastrar o funcionário. Por favor, tente novamente.");
+        }
       })
       .catch((error) => {
         // Tratar erros
         console.error(error);
+        alert("Ocorreu um erro durante o cadastro do funcionário. Por favor, tente novamente mais tarde.");
       });
   }
-  
+
+  document.getElementById("cadastrar-btn").addEventListener("click", cadastrarFuncionario);
+});
